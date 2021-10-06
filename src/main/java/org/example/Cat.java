630fb486910fbb3c4b672d0b9908fc212de25bbd
AntonOvsienko <anton.sokira@gmail.com>
2021-10-06 09:45:45 +0300

    Initial commit

---
+++ src/main/java/org/example/Cat.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Cat implements Pets{
+
+    @Override
+    public void say() {
+        System.out.println("Мяу-мяу");
+    }
+}
---
+++ src/main/java/org/example/Dog.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Dog implements Pets{
+    @Override
+    public void say() {
+        System.out.println("Гав-гав");
+    }
+}
